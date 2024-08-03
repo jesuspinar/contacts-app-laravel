e4ed05e9d75c3e94592e5db80d265e9e74f5d276
Jesus Pinar <not@email.com>
2024-08-03 21:13:49 +0200

    Added delete

--- resources/views/contacts/list.blade.php
+++ resources/views/contacts/list.blade.php
@@ -3,16 +3,23 @@
 @section('content')
   <div class="container pt-4 p-3">
     <div class="row">
-
       @forelse ($contacts as $contact)
         <div class="col-md-4 mb-3">
           <div class="card text-center">
             <div class="card-body">
               <h3 class="card-title text-capitalize">{{ $contact->name }}</h3>
               <p class="m-2">{{ $contact->phone_number }}</p>
-              <a href={{ route('contacts.edit', $contact) }} class="btn btn-secondary mb-2">Edit
+              <a href="{{ route('contacts.edit', $contact) }}" class="btn btn-secondary mb-2">Edit
                 Contact</a>
-              <a href="" class="btn btn-danger mb-2">Delete Contact</a>
+
+              <form action="{{ route('contacts.destroy', $contact) }}" method="POST" class="d-inline">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger mb-2"
+                  onclick="return confirm('Are you sure you want to delete this contact?');">
+                  Delete Contact
+                </button>
+              </form>
             </div>
           </div>
         </div>
